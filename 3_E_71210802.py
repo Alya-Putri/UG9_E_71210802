ef67a0d40ae9d52cad7d5f735e7787b3a0970374
Alya Putri <alya.putri@ti.ukdw.ac.id>
2021-11-12 12:11:13 +0700

    PROGRAM CATATAN DIGITAL

---
+++ 3_E_71210802.py
@@ -0,0 +1,7 @@
+daftarsatu = (input("Masukkan daftar belanja 1: "))
+daftardua = (input("Masukkan daftar belanja 2: "))
+datakesatu = (input("Tambahkan data ke daftar belanja 1: "))
+datakedua = (input("Tambahkan data ke daftar belanja 2: "))
+print(f"Daftar belanja 1 adalah {daftarsatu },{datakesatu}")
+print(f"Daftar belanja 2 adalah {daftardua },{datakedua}")
+
